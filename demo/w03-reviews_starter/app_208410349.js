6bb242745c5674a122beb92817d7bf63b420138f
JKYROC <JKYROC@gmail.com>
2023-03-02 20:31:34 +0800

    W03-P3: implement next button and show the sequence in console.log

--- demo/w03-reviews_starter/app_208410349.js
+++ demo/w03-reviews_starter/app_208410349.js
@@ -27,8 +27,16 @@ const reviews = [
     job: 'the boss',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-  },
+  }
+  // {
+  //   id: 5,
+  //   name: '郭昱霆',
+  //   job: '資訊工程師',
+  //   img: 'https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w03-reviews-starter/about.jpg',
+  //   text: ' 搜集使用者數據及電商商品資料，協助廣告優化師優化廣告及解決客戶回報問題經手50多家客戶，熟悉使用網頁工具，熟練運用不同專案工具達成目標設計協助專案運行的套件，解決公司總客戶數5 ％面臨到的問題熟悉Google Ads、Google Analytics、Meta 等廣告渠道',
+  // },
 ];
+console.log ([reviews.length]);
 
 const img = document.querySelector('#person-img');
 const author = document.querySelector('#author');
@@ -42,11 +50,33 @@ const randomBtn = document.querySelector('.random-btn');
 
 let currentItem = 0;
 
-window.addEventListener('DOMContentLoaded', () =>{
-  const item = reviews[currentItem];
+const showReview = (person) =>{
+  const item = reviews[person];
   console.log('item', item);
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+  showReview(currentItem);
 });
+
+nextBtn.addEventListener('click', () =>{
+  currentItem++;
+  if(currentItem >= reviews.length){
+    currentItem = 0;
+  }
+  console.log('currentItem',)
+  showReview(currentItem);
+})
+prevBtn.addEventListener('click', () =>{
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length -1;
+  }
+  console.log('currentItem',)
+  showReview(currentItem);
+})
