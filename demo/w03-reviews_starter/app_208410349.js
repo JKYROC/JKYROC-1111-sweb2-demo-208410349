85c4db125bad35d02fcaac2d08b82f86d7599f6b
JKYROC <JKYROC@gmail.com>
2023-03-02 20:51:18 +0800

    implement "suprise me" button and show the sequence in console.log

--- demo/w03-reviews_starter/app_208410349.js
+++ demo/w03-reviews_starter/app_208410349.js
@@ -2,39 +2,40 @@
 const reviews = [
   {
     id: 1,
+    name: '郭昱霆',
+    job: '資訊工程師',
+    img: 'https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w03-reviews-starter/about.jpg',
+    text: ' 搜集使用者數據及電商商品資料，協助廣告優化師優化廣告及解決客戶回報問題經手50多家客戶，熟悉使用網頁工具，熟練運用不同專案工具達成目標設計協助專案運行的套件，解決公司總客戶數5 ％面臨到的問題熟悉Google Ads、Google Analytics、Meta 等廣告渠道',
+  },
+  {
+    id: 2,
     name: 'susan smith',
     job: 'web developer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
-    id: 2,
+    id: 3,
     name: 'anna johnson',
     job: 'web designer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
-    id: 3,
+    id: 4,
     name: 'peter jones',
     job: 'intern',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
-    id: 4,
+    id: 5,
     name: 'bill anderson',
     job: 'the boss',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   }
-  // {
-  //   id: 5,
-  //   name: '郭昱霆',
-  //   job: '資訊工程師',
-  //   img: 'https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w03-reviews-starter/about.jpg',
-  //   text: ' 搜集使用者數據及電商商品資料，協助廣告優化師優化廣告及解決客戶回報問題經手50多家客戶，熟悉使用網頁工具，熟練運用不同專案工具達成目標設計協助專案運行的套件，解決公司總客戶數5 ％面臨到的問題熟悉Google Ads、Google Analytics、Meta 等廣告渠道',
-  // },
+
 ];
 console.log ([reviews.length]);
 
@@ -78,5 +79,9 @@ prevBtn.addEventListener('click', () =>{
     currentItem = reviews.length -1;
   }
   console.log('currentItem',)
+  showReview(currentItem);
+})
+randomBtn.addEventListener('click', () =>{
+  currentItem = Math.floor(Math.random() * reviews.length);
   showReview(currentItem);
 })
