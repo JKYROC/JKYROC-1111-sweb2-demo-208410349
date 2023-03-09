917a202bf50d9f14eec96d6f8d3063b9642020a9
JKYROC <JKYROC@gmail.com>
2023-03-09 20:52:48 +0800

    W04-P2: show all data in array menu into html (section-center)

--- demo/w04-menu starter/menu_starter_xx/app_xx.js
+++ demo/w04-menu starter/menu_starter_xx/app_xx.js
@@ -1,20 +1,23 @@
+const category = [   
+    'all','breakfast','lunch','dinner','shakes'
+]
 const menu = [
     {
         id: 1,
         title: "Baked Egg",
         category: "breakfast",
         price: 11.59,
-        image: "./images/my-1.jpegs",
-        remote_image: "",
+        image: "./images/my-1.jpg",
+        remote_image: "https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w04-menu-starter/my-1.jpg",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
     },
     {
         id: 2,
         title: "Steak and fried egg",
         category: "lunch",
-        price: 17.999,
-        image: "./images/my-2.jpegs",
-        remote_image: "",
+        price: 17.99,
+        image: "./images/my-2.jpg",
+        remote_image: "https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w04-menu-starter/my-2.jpg",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
     },
     {
@@ -22,25 +25,25 @@ const menu = [
         title: "Spaghetti with meatballs",
         category: "dinner",
         price: 18.79,
-        image: "./images/my-3.jpegs",
-        remote_image: "",
+        image: "./images/my-3.jpg",
+        remote_image: "https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w04-menu-starter/my-3.jpg",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
     },
     {
         id: 4,
-        title: "Spaghetti with meatballs",
-        category: "dinner",
-        price: 18.79,
-        image: "./images/my-3.jpegs",
-        remote_image: "",
-        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        image: "./images/item-1.jpeg",
+        remote_image: "https://skhsjlwrhersyemrmkib.supabase.co/storage/v1/object/public/javascript/demo-208410349/w04-menu-starter/item-1.jpeg",
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammockfreegan copper mug whatever cold-pressed"      
     },
     {
         id: 5,
         title: "Spaghetti with meatballs",
         category: "dinner",
         price: 18.79,
-        image: "./images/my-3.jpegs",
+        image: "./images/item-5.jpeg",
         remote_image: "",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
     },
@@ -49,12 +52,58 @@ const menu = [
         title: "Spaghetti with meatballs",
         category: "dinner",
         price: 18.79,
-        image: "./images/my-3.jpegs",
+        image: "./images/item-6.jpeg",
         remote_image: "",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
     },
 
 ];
 
+
+
 const sectionCenter = document.querySelector('.section-center');
-const btnContainer = document.querySelector('btn-container');
+const btnContainer = document.querySelector('.btn-container');
+
+const displayMenuButtons = (category) =>{
+    let displayButton = category.map( (category) =>{
+        return`
+        <div class="btn-container">
+        <button type="button" class="filter-btn" data-id="${category}">${category}</button>
+      </div> 
+        `     
+    });
+    
+    console.log('filterItems before join', displayButton);
+    displayButton = displayButton.join('');
+    console.log('filterItems after join',displayButton);
+    btnContainer.innerHTML = displayButton;
+};
+const displayMenuItems = (menu) =>{
+    let displayMenu = menu.map( (item) =>{
+        return`
+        <article class="menu-item">
+          <img src="${item.image}" alt="${item.title}" class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">
+                ${item.desc}
+            </p>
+          </div>
+        </article>
+        `     
+    });
+    console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+};
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+    displayMenuItems(menu);
+    displayMenuButtons(category);
+
+});
