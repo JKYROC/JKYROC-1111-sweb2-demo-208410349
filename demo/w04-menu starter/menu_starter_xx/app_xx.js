6c83ded86e8f137e65fe20f69c2ec49c0c6aee7f
JKYROC <JKYROC@gmail.com>
2023-03-09 19:11:37 +0800

    W04-P1: add 3 images of my meals to menu array

---
+++ demo/w04-menu starter/menu_starter_xx/app_xx.js
@@ -0,0 +1,60 @@
+const menu = [
+    {
+        id: 1,
+        title: "Baked Egg",
+        category: "breakfast",
+        price: 11.59,
+        image: "./images/my-1.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+    {
+        id: 2,
+        title: "Steak and fried egg",
+        category: "lunch",
+        price: 17.999,
+        image: "./images/my-2.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+    {
+        id: 3,
+        title: "Spaghetti with meatballs",
+        category: "dinner",
+        price: 18.79,
+        image: "./images/my-3.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+    {
+        id: 4,
+        title: "Spaghetti with meatballs",
+        category: "dinner",
+        price: 18.79,
+        image: "./images/my-3.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+    {
+        id: 5,
+        title: "Spaghetti with meatballs",
+        category: "dinner",
+        price: 18.79,
+        image: "./images/my-3.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+    {
+        id: 6,
+        title: "Spaghetti with meatballs",
+        category: "dinner",
+        price: 18.79,
+        image: "./images/my-3.jpegs",
+        remote_image: "",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim accusantium natus hic tenetur esse commodi sit perferendis similique dicta earum!"
+    },
+
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('btn-container');
