748a3189e48f4408d2b1f3032f93d37e204c89c4
JKYROC <JKYROC@gmail.com>
2023-03-23 20:06:12 +0800

    W06-P2: temperature convert from C to F

---
+++ demo/w06-array/p2_49/data_49.js
@@ -0,0 +1,54 @@
+export const students = [
+    {
+        id:1,
+        name:'Peter',
+        score: 62
+    },
+    {
+        id:2,
+        name:'James',
+        score: 24
+    },
+    {
+        id:3,
+        name:'Robert',
+        score: 70
+    },
+    {
+        id:4,
+        name:'John',
+        score: 99
+    },
+    {
+        id:5,
+        name:'Micheal',
+        score: 64
+    },
+    {
+        id:6,
+        name:'David',
+        score: 15
+    },
+    {
+        id:7,
+        name:'William',
+        score: 37
+    },
+    {
+        id:8,
+        name:'Richard',
+        score: 26
+    },
+    {
+        id:9,
+        name:'Joseph',
+        score: 82
+    },
+    {
+        id:10,
+        name:'Charles',
+        score: 77
+    },
+]
+
+export const sdata = [90,100,60,40,20,80];
---
+++ demo/w06-array/p2_49/p2_49.js
@@ -0,0 +1,18 @@
+import {students, sdata } from './data_49.js';
+
+console.log('students', students);
+console.log('sdata', sdata);
+
+const students2 = students.map( (student) =>{
+    student.role = 'student';
+    return student;
+})
+console.log('students2' ,students2);
+
+sdata.sort( function(a,b){return b-a});
+console.log('sdata',sdata);
+console.log(`The hightest score ${sdata[0]}`);
+console.log(`The lowest score ${sdata[sdata.length-1]}`);
+
+students.sort(function(a,b){return b.score - a.score});
+console.log('students', students);
