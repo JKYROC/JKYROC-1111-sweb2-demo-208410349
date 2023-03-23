931a703f6176f0d634bbe0c0d1165834a1bf1f94
JKYROC <JKYROC@gmail.com>
2023-03-23 21:00:03 +0800

    W06-P4: compute the average of students and sdata array

--- demo/w06-array/p2_49/p2_49.js
+++ demo/w06-array/p2_49/p2_49.js
@@ -20,3 +20,9 @@ students2.sort(function(a,b){return a.score - b.score});
 console.log('students2 sorted',students2);
 console.log(`The lowest score ${students2[0].score}`);
 console.log(`The hightest score ${students2[students2.length-1].score}`);
+
+const average = students2.reduce( (total,students2,index) =>{
+    console.log('index total', index,total);
+    return total + students2.score;
+},0) / students2.length;
+console.log('average', average);
